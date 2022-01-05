dc4ffad4f581a62cadba408fdfbddf153fdb24e1
NgoDucHoang <97168985+NgoDucHoang@users.noreply.github.com>
2022-01-05 19:27:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    titleWeb: "Chỉ cần cậu nói có",
+    titleWeb: "Chỉ cần cậu nói có :))",
     introTitle: 'Phượng Nguyễn',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
