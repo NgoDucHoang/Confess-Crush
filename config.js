f712136675217ca73a4f2ace0c45b0ecb2f78e0e
NgoDucHoang <97168985+NgoDucHoang@users.noreply.github.com>
2022-01-05 22:21:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Yêu cậu nhiều 😘',
     btnAccept: 'Okiiiii lun <3',
